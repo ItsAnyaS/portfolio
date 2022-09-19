a3f6282cb4f2df38e0616d447464f35c5f4086cb
anastasia <anyasirman@outlook.com>
2022-09-19 11:02:25 -0400

    added noreferrer

--- src/Contact.js
+++ src/Contact.js
@@ -5,8 +5,8 @@ return (
    <span className='span'><ion-icon name="call-outline"></ion-icon><a href="tel:347-825-9968">(347)-825-9968</a></span>
    <span className='span'><ion-icon name="mail-outline"></ion-icon><a href = "mailto: anyasirman@outlook.com">anyasirman@outlook</a></span>
    <div className='social-contianer'>
-   <a href='https://www.linkedin.com/in/anastasiasirman/' target='_blank'><ion-icon name="logo-linkedin"></ion-icon></a>
-   <a href='https://github.com/itsAnyaS' target='_blank'><ion-icon name="logo-github"></ion-icon></a>
+   <a href='https://www.linkedin.com/in/anastasiasirman/' rel="noreferrer" target='_blank'><ion-icon name="logo-linkedin"></ion-icon></a>
+   <a href='https://github.com/itsAnyaS' target='_blank' rel="noreferrer"><ion-icon name="logo-github"></ion-icon></a>
     
     </div>
   </div>
