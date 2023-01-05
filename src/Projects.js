fd33db2f9e7e19cdc32eb7152157dfaede4e3bd1
anastasia <anyasirman@outlook.com>
2023-01-04 19:34:27 -0500

    removed some demo buttons and added demo for callbck

--- src/Projects.js
+++ src/Projects.js
@@ -12,7 +12,7 @@ const Projects = () => {
             With Callbck dancers no longer need to wonder if they got the role or if they were out of the runnings a long time ago.  Callbck creates an all in one ecosystem for companies to post auditions for dancers, send callbcks, mark dancers as hired and remove them from the running.
           </p>
           <button className="project-btn"><a href="https://github.com/ItsAnyaS/Callbck" target='_blanck'>Repo</a></button>
-          <button className="project-btn"><a href="http://www.callbck.com/" target='_blanck'>View Site</a></button>
+          <button className="project-btn"><a href="https://www.youtube.com/watch?v=SWORJYAQhMM" target='_blanck'>View Site</a></button>
               </div>
           <div className='laptop-card3' >
           </div>
@@ -26,7 +26,7 @@ const Projects = () => {
             Banana is a e-commerce site built using postgrSQL, ExpressJS and ReactJS.  The store brings fun back into shopping and making customers go bananas for the great deals.
           </p>
           <button className="project-btn"><a href="https://github.com/ItsAnyaS/banana" target='_blanck'>Repo</a></button>
-          <button className="project-btn"><a href="https://www.youtube.com/" target='_blanck'>View Demo</a></button>
+          {/* <button className="project-btn"><a href="https://www.youtube.com/" target='_blanck'>View Demo</a></button> */}
               </div>
           <div className='laptop-card2' >
 
@@ -42,7 +42,7 @@ const Projects = () => {
           Arther is a website that connect freelancers to cosumers.  Built with ReactJS, Ruby on Rails and postgreSQL Arther is easily scalable.
           </p>
           <button className="project-btn">Repo</button>
-          <button className="project-btn">View Demo</button>
+          {/* <button className="project-btn">View Demo</button> */}
               </div>
           <div className='laptop-card' >
             </div>
