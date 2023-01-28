5b262b913ab62a09cfd1338c9774db8ab0d0afb9
anastasia <anyasirman@outlook.com>
2023-01-28 13:30:22 -0500

    added favicon to resume page

--- src/Nav.js
+++ src/Nav.js
@@ -28,7 +28,7 @@ const Nav = () => {
         </div>
         <a className='nav-item' href='/'>Home</a>
         <a href='#projects' className='nav-item' >Projects</a>
-        <a className='nav-item' href='Anastasia_Sirman_resume.pdf' >Resume</a>
+        <a className='nav-item' target="_blank" href='Anastasia_Sirman_resume.pdf' >Resume</a>
         <a href="#contact"  className='nav-cta-btn'>
           Say Hi!
         </a>
